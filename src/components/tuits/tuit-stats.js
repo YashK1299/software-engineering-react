0482c778c7480a3012938e3dabd9c1c59c81f900
Yash Kothari <kothariy3@gmail.com>
2022-04-08 10:31:37 -0400

    Toggling Likes

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,22 +1,23 @@
-
-const TuitStats = ({
-                       tuit = () => {
-    }
-                   }) => {
-    return (
-        <div className="row mt-2">
-            <div className="col">
-                <i className="far fa-message me-1"/>
-                {tuit.stats && tuit.stats.replies}
-            </div>
-            <div className="col">
-                <i className="far fa-retweet me-1"/>
-                {tuit.stats && tuit.stats.retuits}
-            </div>
-            <div className="col">
-                <i className="far fa-inbox-out"/>
-            </div>
-        </div>
-    );
+const TuitStats = ({tuit, likeTuit}) => {
+  return (
+    <div className="row">
+      ...
+      <div className="col">
+        <span onClick={() => likeTuit(tuit)}>
+        {
+          tuit.stats.likes > 0 &&
+          <i className="fas fa-heart"
+             style={{color: 'red'}}></i>
+        }
+        {
+          tuit.stats.likes <= 0 &&
+          <i className="far fa-heart"></i>
+        }
+        {tuit.stats && tuit.stats.likes}
+        </span>
+      </div>
+      ...
+    </div>
+  );
 }
-export default TuitStats;
+export default TuitStats
